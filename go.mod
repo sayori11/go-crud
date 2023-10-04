a976a87b037034650027fe9e912e1919e8bf9445
Suraj Giri <gsuraj1111@gmail.com>
2023-10-04 12:50:33 +0545

    Swagger docs

--- go.mod
+++ go.mod
@@ -1,16 +1,27 @@
-module hello
+module server
 
 go 1.21.1
 
 require (
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/labstack/echo/v4 v4.11.1
+	github.com/stretchr/testify v1.8.4
+	github.com/swaggo/echo-swagger v1.4.1
+	github.com/swaggo/swag v1.16.2
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.4
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -18,18 +29,23 @@ require (
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server.go
+++ server.go
@@ -3,17 +3,37 @@ package main
 import (
 	"net/http"
 
+	_ "server/docs"
+
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+// @title Products API
+// @version 1.0
+// @description Test API
+
+// @host localhost:1323
+// @BasePath /
+// @schemes http
+
 type (
+	ProductCreate struct {
+		Code  string `json:"code" validate:"required" example:"A45"`
+		Price uint   `json:"price" validate:"required,gte=0,lte=500" example:"200"`
+	}
 	Product struct {
 		gorm.Model
-		Code  string `json:"code" validate:"required"`
-		Price uint   `json:"price" validate:"required,gte=0,lte=500"`
+		ProductCreate
+	}
+	DataResponse[T Product | []Product | string] struct {
+		Data T `json:"data"`
+	}
+	ErrorResponse struct {
+		Error string `json:"error"`
 	}
 	CustomValidator struct {
 		validator *validator.Validate
@@ -31,64 +51,102 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 func getDB() *gorm.DB {
 	dsn := "host=localhost user=postgres password=password dbname=postgres port=5433 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	db.AutoMigrate(&Product{})
 	if err != nil {
 		panic("Failed to connect to db")
 	}
+	db.AutoMigrate(&Product{})
 	return db
 }
 
+// @Summary Get a list of products
+// @Tags Products
+// @Accept */*
+// @Produce json
+// @Success 200 {object} DataResponse[[]Product]
+// @Router /products [get]
 func getProducts(c echo.Context) error {
 	db := getDB()
 	products := []Product{}
 	db.Find(&products)
-	return c.JSON(http.StatusOK, map[string]interface{}{"data": products})
+	return c.JSON(http.StatusOK, DataResponse[[]Product]{Data: products})
 }
 
+// @Summary Insert a product
+// @Tags Products
+// @Accept json
+// @Produce json
+// @Param object body ProductCreate true "Product Body"
+// @Success 200 {object} DataResponse[Product]
+// @Failure 400 {object} ErrorResponse
+// @Router /products [post]
 func insertProduct(c echo.Context) error {
 	db := getDB()
 	var p Product
 	if err := c.Bind(&p); err != nil {
-		return c.String(http.StatusBadRequest, "bad request")
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: "Binding error"})
 	}
 	if err := c.Validate(p); err != nil {
-		return err
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
 	}
 	db.Create(&p)
-	return c.JSON(http.StatusCreated, p)
+	return c.JSON(http.StatusCreated, DataResponse[Product]{Data: p})
 }
 
+// @Summary Retrieve a product
+// @Tags Products
+// @Param id path int true "Product ID"
+// @Accept */*
+// @Produce json
+// @Success 200 {object} DataResponse[Product]
+// @Failure 400 {object} ErrorResponse
+// @Router /products/{id} [get]
 func retrieveProduct(c echo.Context) error {
 	db := getDB()
 	id := c.Param("id")
 	var product Product
 	result := db.First(&product, id)
 	if result.Error != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"data": "Record not found"})
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: "Product not found"})
 	}
-	return c.JSON(http.StatusOK, map[string]interface{}{"data": product})
+	return c.JSON(http.StatusOK, DataResponse[Product]{Data: product})
 }
 
+// @Summary Delete a product
+// @Tags Products
+// @Param id path int true "Product ID"
+// @Accept */*
+// @Produce json
+// @Success 200 {object} DataResponse[string]
+// @Router /products/{id} [delete]
 func deleteProduct(c echo.Context) error {
 	db := getDB()
 	id := c.Param("id")
 	var product Product
 	db.Delete(&product, id)
-	return c.JSON(http.StatusOK, map[string]string{"message": "Deleted successfully"})
+	return c.JSON(http.StatusOK, DataResponse[string]{Data: "Deleted successfully"})
 }
 
+// @Summary Update a product
+// @Tags Products
+// @Param id path int true "Product ID"
+// @Accept json
+// @Produce json
+// @Param object body ProductCreate true "product body"
+// @Success 200 {object} DataResponse[Product]
+// @Failure 400 {object} ErrorResponse
+// @Router /products/{id} [put]
 func updateProduct(c echo.Context) error {
 	db := getDB()
 	id := c.Param("id")
 	var p Product
 	if err := c.Bind(&p); err != nil {
-		return c.String(http.StatusBadRequest, "bad request")
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: "Binding error"})
 	}
 	if err := c.Validate(p); err != nil {
-		return err
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
 	}
 	db.First(&Product{}, id).Updates(&p)
-	return c.JSON(http.StatusCreated, map[string]interface{}{"data": p})
+	return c.JSON(http.StatusOK, DataResponse[Product]{Data: p})
 }
 
 func Sum[T int | float64](x T, y T) T {
@@ -106,5 +164,6 @@ func main() {
 	e.POST("/products", insertProduct)
 	e.DELETE("/products/:id", deleteProduct)
 	e.PUT("/products/:id", updateProduct)
+	e.GET("/swagger/*", echoSwagger.EchoWrapHandler())
 	e.Logger.Fatal(e.Start(":1323"))
 }
